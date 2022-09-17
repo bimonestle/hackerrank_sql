2dfd93c97486d17322bc76b7ee9ad98b4b7db15a
bimonestle <bimonestle@yahoo.co.id>
2022-09-17 14:33:33 +0700

    Hackerrank - SQL - Basic Select

---
+++ name_of_employees.sql
@@ -0,0 +1,14 @@
+-- Write a query that prints a list of employee names
+-- (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+-- The Employee Table
+-- employee_id Int
+-- name Str
+-- months int
+-- salary int
+
+SELECT
+    NAME
+FROM EMPLOYEE
+ORDER BY
+    NAME ASC
